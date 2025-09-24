548fe751fe652cea17058b6a5589153463b818a6
Ridge Kimani <ridgekimani@outlook.com>
2025-09-24 21:24:31 +0300

    chore: update argument values

--- Maths/IsHarshad.js
+++ Maths/IsHarshad.js
@@ -3,22 +3,22 @@
  * A Harshad number (or Niven number) is a positive integer that is divisible by the sum of its own digits.
  *
  * The function checks if a number is a Harshad (Niven) number.
- * @param {any} n - The number to check
+ * @param {any} val - The number to check
  * @returns {boolean} - true if Harshad, false otherwise
  * @example isHarshad(2025) // true
  */
-export const isHarshad = (n) => {
-  const num = Number(n)
+export const isHarshad = (val) => {
+  const num = Number(val)
 
   if (num <= 0 || !Number.isInteger(num)) return false
 
   let sum = 0,
-    temp = n
+    temp = num
 
   while (temp > 0) {
     sum += temp % 10
     temp = Math.floor(temp / 10)
   }
 
-  return n % sum === 0
+  return num % sum === 0
 }
